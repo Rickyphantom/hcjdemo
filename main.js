3679e43999f30a8f995ac9538252e50f7be40d11
Rickyphantom <dbtjsqls2002@naver.com>
2024-03-25 18:38:00 +0900

    11

--- main.js
+++ main.js
@@ -16,17 +16,22 @@ function white() {
 function showhtml() {
   document.getElementById('fig').src = 'img/html.png';
   document.getElementById('desc').innerHTML =
-    '<b>HTML5</b>는 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다.';
+    '<b>HTML</b>는 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다.';
 }
 function showcss() {
   document.getElementById('fig').src = 'img/css.png';
   document.getElementById('desc').innerHTML =
-    '<b>CSS3</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다.';
+    '<b>CSS</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다.';
 }
-function showjs() {
+function showjava() {
   document.getElementById('fig').src = 'img/java.png';
   document.getElementById('desc').innerHTML =
     '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다.';
+}
+function showdada() {
+  document.getElementById('fig').src = 'img/이차는 이제 제껍니다.jpg';
+  document.getElementById('desc').innerHTML =
+    '이돈은 이제 제껍니다 제 맘대로 할 수 있습니다.';
 }
 function hide() {
   document.getElementById('fig').src = '';
